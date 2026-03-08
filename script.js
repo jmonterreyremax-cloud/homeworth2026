0695cc7fbf0ee78dc726b37f805792a3c04313db
John Monterrey <jmonterrey.remax@gmail.com>
2026-03-08 12:30:15 -0400

    Update script.js
    webhook

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("sellerForm");
 const successMessage = document.getElementById("successMessage");
-
+const webhookUrl = "https://hooks.zapier.com/hooks/catch/12452720/uxtawxu/";
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
